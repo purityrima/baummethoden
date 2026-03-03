2ad3844503e9151a15fcc87b03206dbf7b7539b2
Purity  Rima <purityrima atgmail.com>
2026-03-04 00:06:13 +0300

    Add test functions f1 and f2

---
+++ src/test.py
@@ -0,0 +1,12 @@
+def f1(x, y):
+    return x + y
+
+
+def f2(x):
+    return 2 * x
+
+
+if __name__ == "__main__":
+    print("Testing functions:")
+    print("f1(3, 4) =", f1(3, 4))
+    print("f2(5) =", f2(5))
